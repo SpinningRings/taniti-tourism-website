ef84d0f56fa21e60dcec9abf32c64553f6e02066
awagma1 <awagma1@wgu.edu>
2026-08-04 09:18:00 -0400

    Finished basic home page and example article page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { EntertainmentComponent } from './entertainment/entertainment.component'
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { LodgingComponent } from './lodging/lodging.component';
 import { SightseeingComponent } from './sightseeing/sightseeing.component';
+import { ArticleComponent } from './article/article.component';
 
 export const routes: Routes = [{ path: '', component: HomePageComponent },
                                { path: 'transportation', component: TransportationComponent },
@@ -13,5 +14,6 @@ export const routes: Routes = [{ path: '', component: HomePageComponent },
                                { path: 'entertainment', component: EntertainmentComponent },
                                { path: 'contactUs', component: ContactUsComponent },
                                { path: 'lodging', component: LodgingComponent },
-                               { path: 'sightseeing', component: SightseeingComponent }
+                               { path: 'sightseeing', component: SightseeingComponent },
+                               { path: 'article', component: ArticleComponent }
 ];
