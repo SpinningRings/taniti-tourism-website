ef80e86f5c19432b18748dd4ac90652c50b9c464
awagma1 <awagma1@wgu.edu>
2026-08-05 08:26:57 -0400

    Reorganizes main pages for better readability, finished FAQ

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 import { LodgingComponent } from './lodging/lodging.component';
 import { SightseeingComponent } from './sightseeing/sightseeing.component';
 import { ArticleComponent } from './article/article.component';
+import { FAQComponent } from './faq/faq.component';
 
 export const routes: Routes = [{ path: '', component: HomePageComponent },
                                { path: 'transportation', component: TransportationComponent },
@@ -15,5 +16,6 @@ export const routes: Routes = [{ path: '', component: HomePageComponent },
                                { path: 'contactUs', component: ContactUsComponent },
                                { path: 'lodging', component: LodgingComponent },
                                { path: 'sightseeing', component: SightseeingComponent },
-                               { path: 'article', component: ArticleComponent }
+                               { path: 'article', component: ArticleComponent },
+                               { path: 'faq', component: FAQComponent }
 ];
